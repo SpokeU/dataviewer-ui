ab34c63b26bc202ab85ec169e8b3c4912ad29b56
SpokeU <omyshko91@gmail.com>
2021-11-21 14:48:29 +0200

    Add initial layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnectionCardComponent } from './components/connections/connection-card/connection-card.component';
+import { ConnectionsComponent } from './components/connections/connections.component';
+import { CoreModule } from './core/core.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnectionCardComponent,
+    ConnectionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/connections/connection-card/connection-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-connection-card',
+  templateUrl: './connection-card.component.html',
+  styleUrls: ['./connection-card.component.scss']
+})
+export class ConnectionCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
