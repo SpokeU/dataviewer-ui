c4e8bc8d505d64c7042b32393162da30a0e88963
SpokeU <omyshko91@gmail.com>
2021-11-21 18:31:28 +0200

    Collapsible sidebar

--- src/app/core/sidebar/sidebar.component.ts
+++ src/app/core/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, OnInit, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  items: string[] = ['connections', 'queries', 'settings']
+
+  collapsed = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleSidebar(){
+    this.collapsed = !this.collapsed;
   }
 
 }
